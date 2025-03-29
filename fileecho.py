145016921575e14b9d21234e336f22e943ca68ce
Kris Morrow <194855958+kLdrake@users.noreply.github.com>
2025-03-29 19:46:39 -0400

    Update fileecho.py

--- fileecho.py
+++ fileecho.py
@@ -1,3 +1,6 @@
 #Read in the echo.txt file and display it in the terminal using print()
 #This is a guided practice. Either follow the video or your instructor in class.
 
+f = open("echo.txt", "r")
+print(f.read())
+f.close()
