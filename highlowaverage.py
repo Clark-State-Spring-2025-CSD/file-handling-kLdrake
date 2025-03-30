725cd51d970ce7ced767a6f47f89220ec0cc5076
Kris Morrow <194855958+kLdrake@users.noreply.github.com>
2025-03-29 20:35:07 -0400

    Update highlowaverage.py

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,31 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+
+numbers = open("numbers.txt")
+
+total = 0
+avg = 0
+count = 0
+high = float('-inf')
+low = float('inf')
+
+for line in numbers:
+    num = int(line)
+    count += 1
+    n = float(line)
+    total += num
+    if num > high:
+        high = num
+    if num < low:
+        low = num
+
+avg = total / count
+
+
+numbers.close()
+
+print(f"There are {count} numbers in the file.")
+print(f"The average is {avg}.")
+print(f"The highest number is {high}")
+print(f"The lowest number is {low}")
