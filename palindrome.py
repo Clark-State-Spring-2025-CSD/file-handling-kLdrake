9f54db97a3c684696d404287828a299d5e90946f
Kris Morrow <194855958+kLdrake@users.noreply.github.com>
2025-03-29 21:15:21 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,16 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+wordlist = open("words.txt")
+
+count = 0
+
+for line in wordlist:
+    words = line.lower().split()
+    count += sum(w == w[::-1] for w in words)
+
+wordlist.close()
+
+print(f"There are {count} palindromes.")
+
